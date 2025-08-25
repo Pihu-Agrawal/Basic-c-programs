570c02cff356fdddac48d74dc6e2f347b516e53d
Pihu-Agrawal <pihuagrawal038@gmail.com>
2025-08-25 16:02:56 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    char c='a';
+    int x=c+1;
+    scanf("%d",&x);
+    printf("%d",x);
+    return 0;
+
+
+}
+    
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
